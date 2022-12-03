aa80dddabbe62d79e5f014b4856c9bab708e9478
Albert Stróżyński <alb.strozynski@gmail.com>
2022-12-03 12:28:59 +0100

    Day 1

--- Day_1/Main.java
+++ Day_1/Main.java
@@ -1,8 +1,40 @@
 package Day_1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        File input = new File("Day_1/input.txt");
+
+        try {
+            Scanner in = new Scanner(input);
+            String lineAsString;
+            int maxCal = 0, maxPerson = 0;
+            int sumCal = 0, person = 0;
+
+            while (in.hasNextLine()) {
+                lineAsString = in.nextLine();
+
+                if(lineAsString.equals("")) {
+                    if(sumCal > maxCal) {
+                        maxCal = sumCal;
+                        maxPerson = person;
+                    }
+                    sumCal = 0;
+                    person++;
+                }else {
+                    sumCal += Integer.parseInt(lineAsString);
+                }
+            }
+            System.out.println(maxPerson);
+            System.out.println(maxCal);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
     }
 }

4b07f7f48239d3f8d0fa375aa4fc0067d838840c
Albert Stróżyński <alb.strozynski@gmail.com>
2022-12-03 12:55:21 +0100

    Day 1

--- Day_1/Main.java
+++ Day_1/Main.java
@@ -2,6 +2,8 @@ package Day_1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -9,29 +11,33 @@ public class Main {
     public static void main(String[] args) {
 
         File input = new File("Day_1/input.txt");
+        ArrayList<Integer> elves = new ArrayList<>();
+
+        int topElves = 3, topSum = 0;
+        int sumCal = 0;
 
         try {
             Scanner in = new Scanner(input);
             String lineAsString;
-            int maxCal = 0, maxPerson = 0;
-            int sumCal = 0, person = 0;
 
             while (in.hasNextLine()) {
                 lineAsString = in.nextLine();
 
                 if(lineAsString.equals("")) {
-                    if(sumCal > maxCal) {
-                        maxCal = sumCal;
-                        maxPerson = person;
-                    }
+                    elves.add(sumCal);
                     sumCal = 0;
-                    person++;
                 }else {
                     sumCal += Integer.parseInt(lineAsString);
                 }
             }
-            System.out.println(maxPerson);
-            System.out.println(maxCal);
+            Collections.sort(elves);
+            System.out.println("Top 1 cal: "+elves.get(elves.size()-1));
+
+            for(int i=1;i<=topElves;i++) {
+                topSum += elves.get(elves.size()-i);
+            }
+
+            System.out.println("Top "+topElves+" cal sum: "+topSum);
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found");

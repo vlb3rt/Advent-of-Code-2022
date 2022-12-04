fdfc40637ac797d55c13d900793e4c3e2e6e9878
Albert Stróżyński <alb.strozynski@gmail.com>
2022-12-05 00:54:58 +0100

    Day 2

--- Day_2/Main.java
+++ Day_2/Main.java
@@ -15,29 +15,47 @@ public class Main {
         final int rock = 1, paper = 2, scissors = 3;
         final int lose = 0, draw = 3, win = 6;
 
-        Map<String, Integer> results = new HashMap<>();
-        results.put("A X", rock + draw);
-        results.put("A Y", paper + win);
-        results.put("A Z", scissors + lose);
-        results.put("B X", rock + lose);
-        results.put("B Y", paper + draw);
-        results.put("B Z", scissors + win);
-        results.put("C X", rock + win);
-        results.put("C Y", paper + lose);
-        results.put("C Z", scissors + draw);
+        //HashMaps contain key: all possible combinations of input, value: points according to a specific strategy
+        Map<String, Integer> strategy1 = new HashMap<>();
+        Map<String, Integer> strategy2 = new HashMap<>();
+
+        //A, B, C - competitor's choice; X, Y, Z - my choice
+        strategy1.put("A X", rock + draw);          //A, X -> Rock
+        strategy1.put("A Y", paper + win);          //B, Y -> Paper
+        strategy1.put("A Z", scissors + lose);      //C, Z -> Scissors
+        strategy1.put("B X", rock + lose);
+        strategy1.put("B Y", paper + draw);
+        strategy1.put("B Z", scissors + win);
+        strategy1.put("C X", rock + win);
+        strategy1.put("C Y", paper + lose);
+        strategy1.put("C Z", scissors + draw);
+
+        //A, B, C - competitor's choice; X, Y, Z - expected outcome of the game
+        strategy2.put("A X", scissors + lose);      //A -> Rock
+        strategy2.put("A Y", rock + draw);          //B -> Paper
+        strategy2.put("A Z", paper + win);          //C -> Scissors
+        strategy2.put("B X", rock + lose);
+        strategy2.put("B Y", paper + draw);         //X -> lose
+        strategy2.put("B Z", scissors + win);       //Y -> draw
+        strategy2.put("C X", paper + lose);         //Z -> win
+        strategy2.put("C Y", scissors + draw);
+        strategy2.put("C Z", rock + win);
 
         try {
             Scanner in = new Scanner(input);
-            int sum = 0;
+            int sum1 = 0;
+            int sum2 = 0;
             while (in.hasNextLine()) {
-                Integer currentPoint = results.get(in.nextLine());
-                sum += currentPoint;
+                String line = in.nextLine();
+
+                sum1 += strategy1.get(line);
+                sum2 += strategy2.get(line);
             }
-            System.out.println(sum);
+            System.out.println("Points from strategy 1: "+sum1);
+            System.out.println("Points from strategy 2: "+sum2);
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
-
     }
 }

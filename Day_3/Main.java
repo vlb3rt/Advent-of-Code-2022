95cd1daf7e7b5fb7f5959e547e6f31ec72b468f8
Albert Stróżyński <alb.strozynski@gmail.com>
2022-12-05 19:24:15 +0100

    Day 3

--- Day_3/Main.java
+++ Day_3/Main.java
@@ -6,20 +6,33 @@ import java.util.*;
 
 public class Main {
 
-    public static char findSameItem(String line) {
+    public static LinkedList<Character> stringToArrayList(String str) {
+        LinkedList<Character> results = new LinkedList<>();
+        for(char element: str.toCharArray()) {
+            results.add(element);
+        }
+        return results;
+    }
 
-        LinkedList<Character> itesmsFromLeft = new LinkedList<>();
-        LinkedList<Character> itesmsfromRight = new LinkedList<>();
+    public static char findSameItem(String line1, String line2, String line3) {
+
+        LinkedList<Character> items1 = stringToArrayList(line1);
+        LinkedList<Character> items2 = stringToArrayList(line2);
         Set<Character> result = new HashSet<>();
 
-        for(int i=0;i<line.length()/2;i++) {
-            itesmsFromLeft.add(line.charAt(i));
-            itesmsfromRight.add(line.charAt(line.length()-1-i));
-        }
+        if(!line3.equals("")) {
+            LinkedList<Character> items3 = stringToArrayList(line3);
+            for(Character element: items1) {
+                if(items2.contains(element) && items3.contains(element)) {
+                    result.add(element);
+                }
+            }
 
-        for(Character item: itesmsFromLeft) {
-            if(itesmsfromRight.contains(item)){
-                result.add(item);
+        }else {
+            for(Character element: items1) {
+                if(items2.contains(element)) {
+                    result.add(element);
+                }
             }
         }
         return result.toString().substring(1, 2).charAt(0);
@@ -39,18 +52,36 @@ public class Main {
     public static void main(String[] args) {
 
         File input = new File("Day_3/input.txt");
+
         int points = 0;
+
+        String[] lines = new String[3];
+        int counter = 0;
+        int points2 = 0;
 
         try {
             Scanner in = new Scanner(input);
-
+            String line;
             while (in.hasNextLine()) {
-                points += getPoints(findSameItem(in.nextLine()));
+                line = in.nextLine();
+                points += getPoints(findSameItem(line.substring(0, (line.length()/2)), line.substring(line.length()/2), ""));
+
+                if(counter<3) {
+                    lines[counter] = line;
+                    counter++;
+                }
+
+                if(counter == 3) {
+                    points2 += getPoints(findSameItem(lines[0], lines[1], lines[2]));
+                    counter = 0;
+                }
+
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
 
         System.out.println(points);
+        System.out.println(points2);
     }
 }

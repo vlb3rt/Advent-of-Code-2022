5849bea8526a673833a54fa8934dc3c282e53908
Albert Stróżyński <alb.strozynski@gmail.com>
2022-12-06 00:52:19 +0100

    Day 3

--- Day_3/Main.java
+++ Day_3/Main.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class Main {
 
+    //Function turing string into a LinkedList
     public static LinkedList<Character> stringToArrayList(String str) {
         LinkedList<Character> results = new LinkedList<>();
         for(char element: str.toCharArray()) {
@@ -14,20 +15,19 @@ public class Main {
         return results;
     }
 
+    //Function used to find same elements in 2 or 3 strings
     public static char findSameItem(String line1, String line2, String line3) {
-
         LinkedList<Character> items1 = stringToArrayList(line1);
         LinkedList<Character> items2 = stringToArrayList(line2);
         Set<Character> result = new HashSet<>();
 
-        if(!line3.equals("")) {
+        if(!line3.equals("")) { //Condition used to define how many strings are being compared
             LinkedList<Character> items3 = stringToArrayList(line3);
             for(Character element: items1) {
                 if(items2.contains(element) && items3.contains(element)) {
                     result.add(element);
                 }
             }
-
         }else {
             for(Character element: items1) {
                 if(items2.contains(element)) {
@@ -38,6 +38,7 @@ public class Main {
         return result.toString().substring(1, 2).charAt(0);
     }
 
+    //Function calculates points for each character
     public static int getPoints(char element) {
         int valueToSubstractForLowerCase = 96;
         int valueToSubstractForUpperCase = 38;
@@ -53,35 +54,35 @@ public class Main {
 
         File input = new File("Day_3/input.txt");
 
-        int points = 0;
-
+        int points1 = 0, points2 = 0;//Variables store points for each priority
+        int counter = 0;//Variable used to separate 3 lines
         String[] lines = new String[3];
-        int counter = 0;
-        int points2 = 0;
 
         try {
             Scanner in = new Scanner(input);
             String line;
             while (in.hasNextLine()) {
                 line = in.nextLine();
-                points += getPoints(findSameItem(line.substring(0, (line.length()/2)), line.substring(line.length()/2), ""));
 
+                //Counting points for first priority
+                points1 += getPoints(findSameItem(line.substring(0, (line.length()/2)), line.substring(line.length()/2), ""));
+
+                //Counting points for second priority
                 if(counter<3) {
                     lines[counter] = line;
                     counter++;
                 }
-
                 if(counter == 3) {
                     points2 += getPoints(findSameItem(lines[0], lines[1], lines[2]));
                     counter = 0;
                 }
-
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
 
-        System.out.println(points);
-        System.out.println(points2);
+        //Printing results
+        System.out.println("Sum of first priorities: "+points1);
+        System.out.println("Sum of second priorities: "+points2);
     }
 }

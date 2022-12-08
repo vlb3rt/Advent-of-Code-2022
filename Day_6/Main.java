599d8be48f0d158ab27fb3f2adc73634d559c6a2
Albert Stróżyński <alb.strozynski@gmail.com>
2022-12-08 22:51:45 +0100

    Day 6

---
+++ Day_6/Main.java
@@ -0,0 +1,49 @@
+package Day_6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Main {
+
+    public static boolean isMarker(LinkedList<Character> marker, int markerSize) {
+        int occurance = 0;
+        for(Character element : marker) {
+            for(Character x : marker) {
+                if(element.equals(x)) occurance++;
+            }
+        }
+        return occurance == markerSize;
+    }
+
+    public static int loop(String line, int markerSize) {
+        int lineSize = line.length();
+        LinkedList<Character> marker = new LinkedList<>();
+        for(int i=0;i<lineSize-markerSize;i++) {
+            marker.add(line.charAt(i));
+            if(i>=markerSize-1) {
+                if(isMarker(marker, markerSize)) {
+                    return i+1;
+                }else {
+                    marker.remove(0);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        File file = new File("Day_6/input.txt");
+        String line;
+        try {
+            Scanner in = new Scanner(file);
+            line = in.nextLine();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("First marker for length 4: "+loop(line, 4));
+        System.out.println("First marker for length 14: "+loop(line, 14));
+    }
+}
